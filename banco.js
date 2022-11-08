c5d87f12a10d1c76814bb857b0cdad702327521b
BritoJ <68962922+jhoe666@users.noreply.github.com>
2022-11-08 01:46:05 -0500

    Add files via upload

---
+++ banco.js
@@ -0,0 +1,8 @@
+function registrar(){
+    var nombre, apellido, email, telefono;
+    nombre=document.getElementById("nombre").value;
+    apellido=document.getElementById("apellido").value;
+    email=document.getElementById("email").value;
+    telefono=document.getElementById("telefono").value;
+    console.log(nombre, apellido, email, telefono);
+}
